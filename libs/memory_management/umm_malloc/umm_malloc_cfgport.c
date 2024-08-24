147781d9ed603880e0dbc07ca9b254c2a5024196
Franz Flasch <franz.flasch@gmx.at>
2024-08-24 07:34:15 +0200

    Fix c-fft

--- libs/memory_management/umm_malloc/umm_malloc_cfgport.c
+++ libs/memory_management/umm_malloc/umm_malloc_cfgport.c
@@ -1,9 +1,4 @@
-#include <umm_malloc.h>
-#include <umm_malloc_cfg.h>
-
-#define SUPPORT_UMM_MALLOC_BLOCKS (0x2000)
-#define SUPPORT_UMM_MALLOC_HEAP_SIZE (SUPPORT_UMM_MALLOC_BLOCKS * UMM_BLOCK_BODY_SIZE)
-#define UMM_LASTBLOCK ((SUPPORT_UMM_MALLOC_HEAP_SIZE / UMM_BLOCK_BODY_SIZE) - 1)
+#include <umm_malloc_cfgport.h>
 
 /* Start addresses and the size of the heap */
 uint32_t UMM_MALLOC_CFG_HEAP_SIZE = SUPPORT_UMM_MALLOC_HEAP_SIZE;
--- libs/memory_management/umm_malloc/umm_malloc_cfgport.h
+++ libs/memory_management/umm_malloc/umm_malloc_cfgport.h
@@ -3,3 +3,13 @@
  *
  * This file is intentionally left blank
  */
+
+#include <umm_malloc.h>
+#include <umm_malloc_cfg.h>
+
+#define UMM_INFO
+// #define UMM_MAX_CRITICAL_DEPTH_CHECK
+
+#define SUPPORT_UMM_MALLOC_BLOCKS (0x1000)
+#define SUPPORT_UMM_MALLOC_HEAP_SIZE (SUPPORT_UMM_MALLOC_BLOCKS * UMM_BLOCK_BODY_SIZE)
+#define UMM_LASTBLOCK ((SUPPORT_UMM_MALLOC_HEAP_SIZE / UMM_BLOCK_BODY_SIZE) - 1)
