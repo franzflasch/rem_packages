622851a66f8e0055bfdd4bb75a6f2b05416ea562
Franz Flasch <franz.flasch@gmx.at>
2024-11-20 10:32:08 +0100

    Recipes: Update moving avg filter

--- libs/moving_avg_filter/src/moving_avg_filter.h
+++ libs/moving_avg_filter/src/moving_avg_filter.h
@@ -2,22 +2,45 @@
 #define MOVING_AVG_FILTER_H
 
 /************** integer (macro) version, creates a new function for each filter **********/
-#define CREATE_MOVING_AVG_FUNCTION(FUNC_NAME, STAGES) \
-static inline int FUNC_NAME(int sample) { \
-    static int filter_stages = STAGES; \
+#define MVG_AVG_OK 0
+#define MVG_AVG_WAIT 1
+
+#define CREATE_MOVING_AVG_FUNCTION(FUNC_NAME, STAGES, MUL, SHIFT) \
+static inline int FUNC_NAME(int sample, int *filtered_value) \
+{   \
     static int filter_arr[STAGES] = {0}; \
     static int pos = 0; \
     static int curr_val = 0; \
+    static int init_done = 0; \
     \
+    if (!filtered_value) { \
+        for (int i = 0; i < STAGES; i++) \
+            filter_arr[i] = 0; \
+        pos = 0; \
+        curr_val = 0; \
+        init_done = 0; \
+        return MVG_AVG_WAIT; \
+    } \
     curr_val = curr_val - filter_arr[pos] + sample; \
     filter_arr[pos] = sample; \
     \
     pos++; \
     \
-    if(pos >= filter_stages) \
+    if(pos >= STAGES) \
+    { \
+        init_done = 1; \
         pos = 0; \
+    } \
     \
-    return curr_val / filter_stages; \
+    if (SHIFT) \
+        *filtered_value = ((curr_val * MUL) >> SHIFT); \
+    else \
+        *filtered_value = (curr_val / STAGES); \
+    \
+    if(!init_done) \
+        return MVG_AVG_WAIT; \
+    \
+    return MVG_AVG_OK; \
 }
 
 /* Example:
