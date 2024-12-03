20715894b042c7ac9d8ee17ef6fc81088467c31c
Franz Flasch <franz.flasch@ttcontrol.com>
2024-12-03 15:44:05 +0100

    add simple task_queue lib, similar as ANTIRTOS but in C

---
+++ OS/task_queue/src/task_queue.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef void (*task_event_cb)(void *private);
+
+typedef struct task_event_struct
+{
+    task_event_cb task_event;
+    int exec_pending;
+    void *private;
+
+} task_event_td;
+
+typedef struct task_queue_struct
+{
+    task_event_td *task_events;
+    int elements;
+    int read_loc;
+    int write_loc;
+
+} task_queue_td;
+
+void task_queue_init(task_queue_td *task_queue, int elements)
+{
+    task_queue->task_events = calloc(elements, sizeof(task_event_td));
+
+    if(!task_queue->task_events)
+        return;
+
+    task_queue->elements = elements;
+    task_queue->read_loc = 0;
+    task_queue->write_loc = 0;
+}
+
+void task_queue_push(task_queue_td *task_queue, task_event_cb event_cb, void *private)
+{
+    if (!event_cb)
+        return;
+
+    int write_loc = task_queue->write_loc;
+
+    if (write_loc == task_queue->read_loc && task_queue->task_events[write_loc].exec_pending) 
+    {
+        printf("task queue is full!\n");
+        return;
+    }
+
+    task_queue->task_events[write_loc].task_event = event_cb;
+    task_queue->task_events[write_loc].private = private;
+    task_queue->task_events[write_loc].exec_pending = 1;
+
+    task_queue->write_loc = (task_queue->write_loc + 1) % task_queue->elements;
+}
+
+void task_queue_pull(task_queue_td *task_queue)
+{
+    int read_loc = task_queue->read_loc;
+
+    if(!task_queue->task_events[read_loc].exec_pending)
+        return;
+
+    task_queue->task_events[read_loc].task_event(task_queue->task_events[read_loc].private);
+    task_queue->task_events[read_loc].exec_pending = 0;
+
+    task_queue->read_loc = (task_queue->read_loc + 1) % task_queue->elements;
+}
---
+++ OS/task_queue/src/task_queue.h
@@ -0,0 +1,30 @@
+#ifndef TASK_QUEUE_H
+#define TASK_QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef void (*task_event_cb)(void *private);
+
+typedef struct task_event_struct
+{
+    task_event_cb task_event;
+    int exec_pending;
+    void *private;
+
+} task_event_td;
+
+typedef struct task_queue_struct
+{
+    task_event_td *task_events;
+    int elements;
+    int read_loc;
+    int write_loc;
+
+} task_queue_td;
+
+void task_queue_init(task_queue_td *task_queue, int elements);
+void task_queue_push(task_queue_td *task_queue, task_event_cb event_cb, void *private);
+void task_queue_pull(task_queue_td *task_queue);
+
+#endif /* TASK_QUEUE_H */
