61f9d54d0b77a9d5556b74c08fb4d4596980264e
Franz Flasch <franz.flasch@ttcontrol.com>
2025-07-25 08:35:01 +0200

    Update task_queue

--- OS/task_queue/src/task_queue.c
+++ OS/task_queue/src/task_queue.c
@@ -18,7 +18,18 @@ void task_queue_init(task_queue_td *task_queue, int elements)
     task_queue->write_loc = 0;
 }
 
-void task_queue_push(task_queue_td *task_queue, task_event_cb event_cb, void *private)
+void task_queue_deinit(task_queue_td *task_queue)
+{
+    if (task_queue && task_queue->task_events) {
+        free(task_queue->task_events);
+        task_queue->task_events = NULL;
+        task_queue->elements = 0;
+        task_queue->write_loc = 0;
+        task_queue->read_loc = 0;
+    }
+}
+
+void task_queue_push(task_queue_td *task_queue, task_event_cb event_cb, void *priv)
 {
     if (!event_cb)
         return;
@@ -32,7 +43,7 @@ void task_queue_push(task_queue_td *task_queue, task_event_cb event_cb, void *pr
     }
 
     task_queue->task_events[write_loc].task_event = event_cb;
-    task_queue->task_events[write_loc].private = private;
+    task_queue->task_events[write_loc].priv = priv;
     task_queue->task_events[write_loc].exec_pending = 1;
 
     task_queue->write_loc = (task_queue->write_loc + 1) % task_queue->elements;
@@ -45,8 +56,30 @@ void task_queue_pull(task_queue_td *task_queue)
     if(!task_queue->task_events[read_loc].exec_pending)
         return;
 
-    task_queue->task_events[read_loc].task_event(task_queue->task_events[read_loc].private);
+    task_queue->task_events[read_loc].task_event(task_queue->task_events[read_loc].priv);
     task_queue->task_events[read_loc].exec_pending = 0;
 
     task_queue->read_loc = (task_queue->read_loc + 1) % task_queue->elements;
+}
+
+void task_queue_print(task_queue_td *task_queue)
+{
+    int i = 0;
+
+    printf("--- Task Queue Details ---\n");
+    printf("Total elements in queue: %d\n", task_queue->elements);
+    printf("Read location: %d\n", task_queue->read_loc);
+    printf("Write location: %d\n", task_queue->write_loc);
+    printf("--------------------------\n");
+
+    for(i = 0; i < task_queue->elements; i++)
+    {
+        printf("Element %d:\n", i);
+        // Assuming task_event is a function pointer, we can't directly print its value in a meaningful way
+        // other than its memory address, if desired. For now, we'll skip printing the function itself.
+        printf("  exec_pending: %d\n", task_queue->task_events[i].exec_pending);
+        printf("  priv (address): %p\n", task_queue->task_events[i].priv); // Print the address of priv
+        printf("\n");
+    }
+    printf("--- End of Task Queue Details ---\n");
 }
--- OS/task_queue/src/task_queue.h
+++ OS/task_queue/src/task_queue.h
@@ -1,13 +1,13 @@
 #ifndef TASK_QUEUE_H
 #define TASK_QUEUE_H
 
-typedef void (*task_event_cb)(void *private);
+typedef void (*task_event_cb)(void *priv);
 
 typedef struct task_event_struct
 {
     task_event_cb task_event;
     int exec_pending;
-    void *private;
+    void *priv;
 
 } task_event_td;
 
@@ -21,7 +21,9 @@ typedef struct task_queue_struct
 } task_queue_td;
 
 void task_queue_init(task_queue_td *task_queue, int elements);
-void task_queue_push(task_queue_td *task_queue, task_event_cb event_cb, void *private);
+void task_queue_deinit(task_queue_td *task_queue);
+void task_queue_push(task_queue_td *task_queue, task_event_cb event_cb, void *priv);
 void task_queue_pull(task_queue_td *task_queue);
+void task_queue_print(task_queue_td *task_queue);
 
 #endif /* TASK_QUEUE_H */
