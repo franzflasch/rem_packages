05e2c17b23371d89c30122e32f9b067c8cdaea88
Franz Flasch <franz.flasch@ttcontrol.com>
2025-08-07 05:43:50 +0200

    task_queue: make it ISR safe

--- OS/task_queue/src/task_queue.c
+++ OS/task_queue/src/task_queue.c
@@ -1,21 +1,39 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 #include <task_queue.h>
 
-void task_queue_init(task_queue_td *task_queue, int elements)
+__attribute__((weak)) void task_queue_enter_critical_section(void)
 {
-    task_queue->task_events = calloc(elements, sizeof(task_event_td));
+    return;
+}
 
-    if(!task_queue->task_events)
-    {
-        printf("could not alloc task_events\n");
-        return;
+__attribute__((weak)) void task_queue_exit_critical_section(void)
+{
+    return;
+}
+
+__attribute__((weak)) void task_queue_printf(const char *fmt, ...)
+{
+    (void)fmt;
+}
+
+task_queue_ret task_queue_init(task_queue_td *task_queue, int elements)
+{
+    if (!task_queue || elements <= 0)
+        return TASK_QUEUE_ERR;
+
+    task_queue->task_events = calloc(elements, sizeof(task_event_td));
+    if (!task_queue->task_events) {
+        task_queue_printf("could not alloc task_events\n\r");
+        return TASK_QUEUE_ERR;
     }
 
     task_queue->elements = elements;
     task_queue->read_loc = 0;
     task_queue->write_loc = 0;
+    task_queue->nr_pending = 0;
+
+    return TASK_QUEUE_OK;
 }
 
 void task_queue_deinit(task_queue_td *task_queue)
@@ -29,57 +47,89 @@ void task_queue_deinit(task_queue_td *task_queue)
     }
 }
 
-void task_queue_push(task_queue_td *task_queue, task_event_cb event_cb, void *priv)
+task_queue_ret task_queue_push(task_queue_td *task_queue, task_event_cb event_cb, void *priv)
 {
     if (!event_cb)
-        return;
+        return TASK_QUEUE_ERR;
 
-    int write_loc = task_queue->write_loc;
+    size_t write_loc = task_queue->write_loc;
 
-    if (write_loc == task_queue->read_loc && task_queue->task_events[write_loc].exec_pending)
+    task_queue_enter_critical_section();
+    if ((write_loc == task_queue->read_loc) && task_queue->task_events[write_loc].exec_pending)
     {
-        printf("task queue is full!\n");
-        return;
+        // task_queue_printf("task queue is full!\n\r");
+        task_queue_exit_critical_section();
+        return TASK_QUEUE_ERR;
     }
 
     task_queue->task_events[write_loc].task_event = event_cb;
     task_queue->task_events[write_loc].priv = priv;
-    task_queue->task_events[write_loc].exec_pending = 1;
 
+    task_queue->task_events[write_loc].exec_pending = 1;
+    task_queue->nr_pending++;
     task_queue->write_loc = (task_queue->write_loc + 1) % task_queue->elements;
+
+    task_queue_exit_critical_section();
+
+    return TASK_QUEUE_OK;
 }
 
 void task_queue_pull(task_queue_td *task_queue)
 {
-    int read_loc = task_queue->read_loc;
+    size_t read_loc = task_queue->read_loc;
 
+    task_queue_enter_critical_section();
     if(!task_queue->task_events[read_loc].exec_pending)
+    {
+        task_queue_exit_critical_section();
         return;
+    }
+    task_queue_exit_critical_section();
 
     task_queue->task_events[read_loc].task_event(task_queue->task_events[read_loc].priv);
-    task_queue->task_events[read_loc].exec_pending = 0;
 
+    task_queue_enter_critical_section();
+    task_queue->task_events[read_loc].exec_pending = 0;
+    task_queue->nr_pending = (task_queue->nr_pending > 0) ? task_queue->nr_pending - 1 : 0;
     task_queue->read_loc = (task_queue->read_loc + 1) % task_queue->elements;
+    task_queue_exit_critical_section();
 }
+
+int task_queue_get_pending(task_queue_td *task_queue)
+{
+    task_queue_enter_critical_section();
+    size_t pending = task_queue->nr_pending;
+    task_queue_exit_critical_section();
+    return pending;
+}
+
+int task_queue_get_free(task_queue_td *task_queue)
+{
+    task_queue_enter_critical_section();
+    size_t free = task_queue->elements-task_queue->nr_pending;
+    task_queue_exit_critical_section();
+    return free;
+}
+
 
 void task_queue_print(task_queue_td *task_queue)
 {
     int i = 0;
 
-    printf("--- Task Queue Details ---\n");
-    printf("Total elements in queue: %d\n", task_queue->elements);
-    printf("Read location: %d\n", task_queue->read_loc);
-    printf("Write location: %d\n", task_queue->write_loc);
-    printf("--------------------------\n");
+    task_queue_printf("--- Task Queue Details ---\n\r");
+    task_queue_printf("Total elements in queue: %d\n\r", task_queue->elements);
+    task_queue_printf("Read location: %d\n\r", task_queue->read_loc);
+    task_queue_printf("Write location: %d\n\r", task_queue->write_loc);
+    task_queue_printf("--------------------------\n\r");
 
     for(i = 0; i < task_queue->elements; i++)
     {
-        printf("Element %d:\n", i);
+        task_queue_printf("Element %d:\n\r", i);
         // Assuming task_event is a function pointer, we can't directly print its value in a meaningful way
         // other than its memory address, if desired. For now, we'll skip printing the function itself.
-        printf("  exec_pending: %d\n", task_queue->task_events[i].exec_pending);
-        printf("  priv (address): %p\n", task_queue->task_events[i].priv); // Print the address of priv
-        printf("\n");
+        task_queue_printf("  exec_pending: %d\n\r", task_queue->task_events[i].exec_pending);
+        task_queue_printf("  priv (address): %p\n\r", task_queue->task_events[i].priv); // Print the address of priv
+        task_queue_printf("\n\r");
     }
-    printf("--- End of Task Queue Details ---\n");
+    task_queue_printf("--- End of Task Queue Details ---\n\r");
 }
--- OS/task_queue/src/task_queue.h
+++ OS/task_queue/src/task_queue.h
@@ -1,6 +1,14 @@
 #ifndef TASK_QUEUE_H
 #define TASK_QUEUE_H
 
+#include <stdio.h>
+
+typedef enum
+{
+    TASK_QUEUE_OK = 0,
+    TASK_QUEUE_ERR
+} task_queue_ret;
+
 typedef void (*task_event_cb)(void *priv);
 
 typedef struct task_event_struct
@@ -8,22 +16,34 @@ typedef struct task_event_struct
     task_event_cb task_event;
     int exec_pending;
     void *priv;
-
 } task_event_td;
 
 typedef struct task_queue_struct
 {
     task_event_td *task_events;
-    int elements;
-    int read_loc;
-    int write_loc;
+    size_t elements;
+    size_t read_loc;
+    size_t write_loc;
+    size_t nr_pending;
 
 } task_queue_td;
 
-void task_queue_init(task_queue_td *task_queue, int elements);
+/*
+ * This task queue is designed for single-producer, single-consumer usage.
+ * All functions are non-blocking and assume either thread or interrupt context.
+ * The caller is responsible for implementing task_queue_enter_critical_section()
+ * and task_queue_exit_critical_section().
+ *
+ * The caller is responsible for providing valid, non-NULL pointers to all functions,
+ * except task_queue_init(), which checks its arguments.
+ */
+
+task_queue_ret task_queue_init(task_queue_td *task_queue, int elements);
 void task_queue_deinit(task_queue_td *task_queue);
-void task_queue_push(task_queue_td *task_queue, task_event_cb event_cb, void *priv);
+task_queue_ret task_queue_push(task_queue_td *task_queue, task_event_cb event_cb, void *priv);
 void task_queue_pull(task_queue_td *task_queue);
+int task_queue_get_pending(task_queue_td *task_queue);
+int task_queue_get_free(task_queue_td *task_queue);
 void task_queue_print(task_queue_td *task_queue);
 
 #endif /* TASK_QUEUE_H */
